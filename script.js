d2b0940c64abcf215e7cfb9603e6ffec10508b76
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-29 16:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+var inputName=document.querySelector("#name").value;
+var inputYear=document.querySelector("#year").value;
+function ram() {
+	if(inputName=="" && inputYear==""){
+	document.querySelector("#url").innerHTML="https://localhost:8080/?";
+		
+	}
+	else if(inputName=="" && inputYear!=""){
+document.querySelector("#url").textContent="https://localhost:8080/?year=";
+	}
+	else if(inputName!="" && inputYear==""){
+document.querySelector("#url").innerHTML="https://localhost:8080/?"+inputName;
+	}
+	else if(inputName!="" && inputYear!=""){
+document.querySelector("#url").innerHTML="https://localhost:8080/?"+"name="+inputName+"&year="+inputYear;
+	}
+}
